5b116c97f1650dd6c2fbca183243820adcd9f933
Silence AI <princedibloni4@gmail.com>
2023-07-31 12:18:01 +0000

    cors modified

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use((req: any, res: { header: (arg0: string, arg1: string) => void; }, next: () => void) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
+  var whitelist = ['https://digitloop.netlify.app', 'digitloop.netlify.app'];
+  app.enableCors({
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      console.log("allowed cors for:", origin)
+      callback(null, true)
+    } else {
+      console.log("blocked cors for:", origin)
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+  methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
+  credentials: true,
   });
   await app.listen(process.env.PORT || 3000);
 }
