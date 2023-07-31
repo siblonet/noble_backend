1f9bbd40eda03498ce011de29bb1109b2bc27fd3
Silence AI <princedibloni4@gmail.com>
2023-07-31 13:18:36 +0000

    done

--- src/main.ts
+++ src/main.ts
@@ -3,31 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-    if (req.method === 'OPTIONS') {
-      res.status(200).end();
-      return;
-    }
-    return await fn(req, res);
-  };
-
-  const handler = (req, res) => {
-    const d = new Date()
-    res.end(d.toString())
-  };
-
-  // Use the allowCors middleware
-  app.use(allowCors(handler));
+  app.enableCors();
 
   await app.listen(process.env.PORT || 3000);
 }
-
 bootstrap();
