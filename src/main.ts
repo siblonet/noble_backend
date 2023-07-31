dbd77f2da1a82ae1d9dd15e2c22e8704dcbb1eca
Silence AI <princedibloni4@gmail.com>
2023-07-31 12:14:06 +0000

    done

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-
+  app.use((req: any, res: { header: (arg0: string, arg1: string) => void; }, next: () => void) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
