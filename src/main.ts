c98f2adc7970a5b92992baf9c275840f767bd23f
Silence AI <princedibloni4@gmail.com>
2023-07-31 13:09:59 +0000

    corrected by chatgpt

--- src/main.ts
+++ src/main.ts
@@ -3,31 +3,31 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  var whitelist = ['https://digitloop.netlify.app', 'digitloop.netlify.app'];
 
-  const allowCors = (fn: { (req: any, res: any): void; (arg0: any, arg1: any): any; }) => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader(
       'Access-Control-Allow-Headers',
       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
+    );
     if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
+      res.status(200).end();
+      return;
     }
-    return await fn(req, res)
-  }
+    return await fn(req, res);
+  };
 
-  const handler = (_req: any, res: { end: (arg0: string) => void; }) => {
+  const handler = (req, res) => {
     const d = new Date()
     res.end(d.toString())
-  }
-  app.enableCors(allowCors(handler));
+  };
+
+  // Use the allowCors middleware
+  app.use(allowCors(handler));
 
   await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
