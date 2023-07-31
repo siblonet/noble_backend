cfd3cd4c54ffc4374069f33abc8dfa7a4fe91af2
Silence AI <princedibloni4@gmail.com>
2023-07-31 12:54:49 +0000

    cores in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,30 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   var whitelist = ['https://digitloop.netlify.app', 'digitloop.netlify.app'];
-  app.enableCors({
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      console.log("allowed cors for:", origin)
-      callback(null, true)
-    } else {
-      console.log("blocked cors for:", origin)
-      callback(new Error('Not allowed by CORS'))
+
+  const allowCors = (fn: { (req: any, res: any): void; (arg0: any, arg1: any): any; }) => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
     }
-  },
-  allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-  methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
-  credentials: true,
-  });
+    return await fn(req, res)
+  }
+
+  const handler = (_req: any, res: { end: (arg0: string) => void; }) => {
+    const d = new Date()
+    res.end(d.toString())
+  }
+  app.enableCors(allowCors(handler));
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
