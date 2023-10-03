1618e8190328263abbb36c2ce7f3af7b3dcfbdae
Silence-Mac <princedibloni4@gmail.com>
2023-10-03 00:09:51 +0000

    done

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,38 +1,77 @@
 import { Controller, Get, Query, Post, Body, Param, Put, Delete } from '@nestjs/common';
 import { Article } from './entities/activity.entity';
 import { ActivityService } from './home.service';
+import axios from 'axios';
+import * as fs from 'fs';
+
+const credentialsPath = './cloudstore.json'; // Adjust the path accordingly
+
+const downloadCredentialsFile = async () => {
+  try {
+    const response = await axios.get('https://storage.googleapis.com/seeme-7a462.appspot.com/cloudstore.json');
+    fs.writeFileSync(credentialsPath, JSON.stringify(response.data));
+  } catch (error) {
+    throw new Error('Error downloading credentials file');
+  }
+};
+
+
+
+async function checkIfFileExists(filePath: string): Promise<boolean> {
+  return new Promise<boolean>((resolve) => {
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+      resolve(!err); // If no error, the file exists
+    });
+  });
+}
 
 
 @Controller('boutique')
 export class ActivityController {
   constructor(private activityService: ActivityService) { }
-  /*
+
+
+  async checkFileExists(): Promise<{ exists: boolean }> {
+    const exists = await checkIfFileExists(credentialsPath);
+    return { exists };
+  }
+
+
+  @Post('uploadImage')
+  async createImage(@Body() base64Data: any): Promise<{ ima: String }> {
+    const deer = await this.checkFileExists();
+
+    if (!deer.exists) {
+      downloadCredentialsFile();
+      return await this.activityService.createImageWithDelay(base64Data);
+    } else {
+      return await this.activityService.createImage(base64Data);
+    }
+  }
+
+
+
+
   @Post()
-  create(@Body() article: Article) {
-    return this.activityService.create(article);
-  }*/
+  async create(@Body() article: Article) {
+    return await this.activityService.create(article);
+
+  }
 
   @Get()
   async allArticles(): Promise<Article[]> {
     return await this.activityService.allArticles();
   }
-
-
+  // Update the existing PUT route to handle article update
   @Put('/:id')
-  async updateArticle(@Param('id') id: string, @Body() article: any): Promise<Article> {
+  async updateArticle(@Param('id') id: string, @Body() article: Article): Promise<Article> {
     return this.activityService.updateArticles(id, article);
   }
 
-   // Update the existing DELETE route to handle article removal
+  // Update the existing DELETE route to handle article removal
   @Delete('/:id')
   async removeArticle(@Param('id') id: string): Promise<void> {
     return this.activityService.removeArticle(id);
-  };
-/*
-  @Post('deleteim')
-  async deleteImage(@Body() emages: any): Promise<string> {
-    this.activityService.deleteImage(emages);
-    return "deleted";
-  }*/
+  }
 }
 
--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -2,48 +2,28 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Article } from './entities/activity.entity';
-import axios from 'axios';
-import * as fs from 'fs';
-import { Storage } from '@google-cloud/storage';
 import { v4 as uuidv4 } from 'uuid';
-
-// Use the generated UUID in your NestJS application as needed
+import { Storage } from '@google-cloud/storage';
 
 const credentialsPath = './cloudstore.json'; // Adjust the path accordingly
-
-const downloadCredentialsFile = async () => {
-  try {
-    const response = await axios.get('https://storage.googleapis.com/seeme-7a462.appspot.com/cloudstore.json');
-    fs.writeFileSync(credentialsPath, JSON.stringify(response.data));
-  } catch (error) {
-    throw new Error('Error downloading credentials file');
-  }
-};
-
-downloadCredentialsFile();
 
 const storage = new Storage({
   keyFilename: credentialsPath,
 });
+
 
 @Injectable()
 export class ActivityService {
   private bucketName = 'seeme-7a462.appspot.com';
   constructor(@InjectModel('Boutique') private boutiqueModel: Model<Article>) { }
 
+  async createImage(imagefolder: any): Promise<{ ima: String }> {
 
-
-
-  async create(article: any): Promise<string> {
-    const imago = [];
-    const generatedUuid = this.generateUuid();
-
+    const generatedUuid = this.generateUuid() + imagefolder.nam;
 
     const bucket = storage.bucket(this.bucketName);
-    //console.log(article.image[0].nam);
-    const file = bucket.file(generatedUuid + article.image[0].nam);
-
-    const imageBuffer = Buffer.from(article.image[0].ima, 'base64');
+    const file = bucket.file(generatedUuid);
+    const imageBuffer = Buffer.from(imagefolder.ima, 'base64');
 
     file.save(imageBuffer, {
       metadata: {
@@ -51,37 +31,41 @@ export class ActivityService {
       },
     });
 
-    const publicUrl = `https://storage.googleapis.com/${this.bucketName}/${generatedUuid + article.image[0].nam}`;
-    //console.log(publicUrl);
-
-    /*
-        article.image.forEach(ee => {
-          const bucket = storage.bucket(this.bucketName);
-          console.log(ee.nam);
-          const file = bucket.file(generatedUuid+ee.nam);
-    
-          const imageBuffer = Buffer.from(ee.ima, 'base64');
-      
-            // Upload the buffer to the bucket
-            file.save(imageBuffer, {
-              metadata: {
-                contentType: 'image/jpeg',
-              },
-            });
-
-    const publicUrl = `https://storage.googleapis.com/${this.bucketName}/${generatedUuid + ee.nam}`;
-
-    imago.push({ ima: publicUrl })
-  });
-
-    article.image = imago;
-    await this.boutiqueModel.create(article);
-    */
-    return 'Article created successfully';
+    const publicUrl = `https://storage.googleapis.com/${this.bucketName}/${generatedUuid}`;
+    return { ima: publicUrl }
   }
 
+  async createImageWithDelay(imagefolder: any): Promise<{ ima: String }> {
+    let imo;
+
+    const generatedUuid = this.generateUuid() + imagefolder.nam;
+
+    setTimeout(() => {
+      const bucket = storage.bucket(this.bucketName);
+      const file = bucket.file(generatedUuid);
+      const imageBuffer = Buffer.from(imagefolder.ima, 'base64');
+  
+      file.save(imageBuffer, {
+        metadata: {
+          contentType: 'image/jpeg',
+        },
+      });
+  
+      imo = `https://storage.googleapis.com/${this.bucketName}/${generatedUuid}`;
+    }, 5000);
+
+    return { ima: imo }
+  }
+
+  async create(article: Article): Promise<Article> {
+    return await this.boutiqueModel.create(article);
+  }
+
+
+
+
   generateUuid(): string {
-    return uuidv4(); // Assuming you have imported uuidv4 from the uuid package
+    return uuidv4();
   }
 
   async allArticles(): Promise<Article[]> {
@@ -100,32 +84,5 @@ export class ActivityService {
 
   async removeArticle(id: string) {
     await this.boutiqueModel.findByIdAndRemove(id);
-  }
-
-
-
-
-  async uploadImage(imdata: { fol: any, nam: any }): Promise<string> {
-    const bucket = storage.bucket(this.bucketName);
-    const file = bucket.file(imdata.nam);
-
-    // Convert base64 data to a buffer
-    const imageBuffer = Buffer.from(imdata.fol, 'base64');
-
-    try {
-      // Upload the buffer to the bucket
-      await file.save(imageBuffer, {
-        metadata: {
-          contentType: 'image/jpeg',
-        },
-      });
-
-      const publicUrl = `https://storage.googleapis.com/${this.bucketName}/${imdata.nam}`;
-
-      return publicUrl; //
-    } catch (error) {
-      console.error('Error uploading image:', error);
-      throw new Error('Error uploading image');
-    }
   }
 }
