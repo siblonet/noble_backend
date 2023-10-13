8c605d454b3efa675b808f2b4b231968b6abf876
Silence-Mac <princedibloni4@gmail.com>
2023-10-13 05:09:05 +0000

    done

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,55 +1,13 @@
 import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
 import { Article } from './entities/activity.entity';
 import { ActivityService } from './home.service';
-import * as fs from 'fs';
-import { MineindService } from 'src/mineind/mineind.service';
-import * as AWS from 'aws-sdk';
-
-async function checkIfFileExists(bucketName: string, key: string): Promise<boolean> {
-  const s3 = new AWS.S3();
-
-  try {
-    await s3.headObject({ Bucket: bucketName, Key: key }).promise();
-    return true;  // File exists
-  } catch (error) {
-    if (error.code === 'NotFound') {
-      return false;  // File doesn't exist
-    }
-    throw error;  // Other error occurred
-  }
-}
-
-
 
 @Controller('boutique')
 export class ActivityController {
-  constructor(private activityService: ActivityService,
-    private readonly mineindService: MineindService) { }
-
-  async checkFileExists(): Promise<boolean> {
-    const exists = await checkIfFileExists('cyclic-zany-plum-bear-us-east-1', 'cloudstore.json');
-    return exists;
-  }
+  constructor(private activityService: ActivityService) { }
 
   @Post('uploadImage')
   async createImage(@Body() base64Data: any): Promise<{ ima: String }> {
-    const exists = await checkIfFileExists(this.mineindService.thisiswhat("XBXORX:AZMB:KOFN:YVZI:FH:VZHG:0"), 'cloudstore.json');
-
-    if (!exists) {
-      const s3 = new AWS.S3();
-      const params = {
-        Bucket: this.mineindService.thisiswhat("XBXORX:AZMB:KOFN:YVZI:FH:VZHG:0"), // Replace with your S3 bucket name
-        Key: 'cloudstore.json',
-        Body: JSON.stringify(this.mineindService.credentialReform())
-      };
-
-      try {
-        await s3.upload(params).promise();
-      } catch (error) {
-        throw new Error('Error uploading credentials to S3');
-      }
-    }
-
     return await this.activityService.createImage(base64Data);
   }
 
--- src/home/home.service.ts
+++ src/home/home.service.ts
@@ -4,31 +4,30 @@ import { Model } from 'mongoose';
 import { Article } from './entities/activity.entity';
 import { v4 as uuidv4 } from 'uuid';
 import { Storage } from '@google-cloud/storage';
-import * as AWS from 'aws-sdk';
 import { MineindService } from 'src/mineind/mineind.service';
-
-
 
 @Injectable()
 export class ActivityService {
   private bucketName = 'seeme-7a462.appspot.com';
-  private awsS3 = new AWS.S3();
 
-  constructor(@InjectModel('Boutique') private boutiqueModel: Model<Article>,
+  constructor(
+    @InjectModel('Boutique') private boutiqueModel: Model<Article>,
     private readonly mineindService: MineindService) { }
 
-  private async downloadCredentialsFromS3(): Promise<any> {
-    const params = { Bucket: this.mineindService.thisiswhat("XBXORX:AZMB:KOFN:YVZI:FH:VZHG:0"), Key: 'cloudstore.json' };
-    const result = await this.awsS3.getObject(params).promise();
-
-    // Assuming credentials are stored as a JSON string in S3
-    const retorrr = JSON.parse(result.Body.toString());
-    console.log(retorrr);
-    return retorrr;
-  }
-
   private async initializeGoogleCloudStorage(): Promise<Storage> {
-    const credentials = await this.downloadCredentialsFromS3();
+    const credentials = {
+			"type": this.mineindService.thisiswhat("HVIERXV*ZXXLFMG"),
+			"project_id": this.mineindService.thisiswhat("HVVNV:4Z759"),
+      "private_key_id": this.mineindService.thisiswhat("84819ZU598Y5W05X04V1VZ0Y24YY3U23493Z37XY"),
+			"private_key": `-----BEGIN PRIVATE KEY-----\n${this.mineindService.thisiswhat("nrrvEjryzwzmyTPJSPRt2D1yzjvuzzhxypXDTThQzTvzzLryzjxqWg3Kv3dfZfL3")}\nl1vM0LJPBURSB2Hm25y/30Mn7qO5RB8NeC11YiJqCTCQRKc9rTHzmEjoE8nyeXwi\nSLK22zGXMnx751vF7rOoUsNHBv5DhYEt9+aSByyW0utb89v9n51g9lSx007fe8M6\n2+wGhLYlc1WvS0+ulvUmE08Lck02d37Mr2OO/aDM6MkhJEe/VA20p+620BW3i+Zj\nKLzKJFboOxLSaYMYKkOrVJFqKPeVGzdtYUgxpNJtfYf5dviF5T4fUbWsTNm/sQP5\nnQ7Li/hHUqeAZf86DjTtw/EZfgy4jh6DLof7xys3vWZ4x6Il+exJq9ISN/NGBPsI\n36ZTo3UFAgMBAAECggEAETGKuHMiLCmtYP3XvDtAinTQsmf8/Xjr1u4wwr5tWT+l\nmLQ3D286ecPzgKAtwR+4V01tlSXghUVf2xP30rqBLfXhL0hlqNDuRPgxN+GujTiW\nS28uNobSM31ndiV/F27PKzE0r9eMDpU2+tZGZBxWgkG2boo/icmVGXGh2f8unEjQ\nND9yiyotU44pxLx9FT7G4pivoicquFHoZ/Azma7LGKjzB8rLQnbX7l6nhTaLKvXS\nttcqUSSJPa0ii6zWjpIxD5eh/03Ydce9rWy69PTouPnERk4aggiq2YG/2cYKy+zq\nZuXfteIEzTQcn/B68KP+Zuo22PHOLIdxEBNbMl4GQQKBgQC8LDGanBYe3m80OoiA\n1JbslLt2zdxQElZnFWt0OhcZNLVzJ6ThXxbdqYDrYeXhcRFAcWLmCbaPJBNG9sD7\nrjMA1pvPqOu4/jSzpvvYzDaXW9WSh8WD8+L1mEQcF972bQ1U2pnDUCPOxI4mydOT\nmiemy3O25hPxTcWZoopGgV5tJQKBgQC7AU/uf42RdfJhjD1Am/PKr8sry1J2Yu/d\ndsleLUMSN16Y2gHqxnRGMZbBYJ9jOsIh702HSxVlfdY8rPmnWx2pw8sxouL4U22f\nL7/xqZUwhLrUWyiFY1TVHlyEN1K23Co56scvI0j2gu08ZxuQuHsgIV8iXaJHUacW\n1wKn3BmSYQKBgQC3Tk3cESUzWPf3ZvXGmlGTkae0qgFuNw7YSHNuu4/4y/HehcYG\njb0WnaKqqalond5yaoIvGVMvybLFtGZ2RlIFBQqvlhxbD7eDMq2vme+sLpe0sHJm\nglSbboMa4t4eLEmKWZDkRNM8/xiDDKUA6nCp+RN2Hlda/6n4afBNkoTLyQKBgDQk\nv4b/JhhjTPyKQCId2jKwJfTGMu4z1DJqhuets0AHFGeGknZGc/GGrkSpjof6wFr6\nASIye3rbRbuRd+OEcpb9s1DZ3HPlv3FdvSXDZYgr2nSQHpJjCiqK1r0/N98pMNUJ\ndBTev9+Mzl3DWlpWNm7VH1PTzAYa7H5f4auwoajBAoGAUCmW9KPz/CT/Y5pZ8ubZ\n7fjeBumAlRVsXWiOxGmeInnFkLO9u7DVXXJkGnsSwPLaoxGQJo8scBjXBepesSPA\nHl635CnFWyey51nUvirdnE/95YFN7y0JLV3Jbh8rCuCKA09B3D/VH1nMqcEXqNnI\nfWPukMHgXmGvJgiMnXzT0Vw=\n-----END PRIVATE KEY-----\n`,
+      "client_email": this.mineindService.thisiswhat("MLYOVHSLK`HVVNV:4Z759&RZN&THVIERXVZXXLFMG&XLN"),
+			"client_id": this.mineindService.thisiswhat("011953168475223620145"),
+			"auth_uri": this.mineindService.thisiswhat("SGGKH-²²ZXXLFMGH&TLLTOV&XLN²L²LZFGS9²ZFGS"),
+			"token_uri": this.mineindService.thisiswhat("SGGKH-²²LZFGS9&TLLTOVZKRH&XLN²GLPVM"),
+			"auth_provider_x509_cert_url": this.mineindService.thisiswhat("SGGKH-²²DDD&TLLTOVZKRH&XLN²LZFGS9²E0²XVIGH"),
+			"client_x509_cert_url": this.mineindService.thisiswhat("SGGKH-²²DDD&TLLTOVZKRH&XLN²ILYLG²E0²NVGZWZGZ²C612²MLYOVHSLK[71HVVNV:4Z759&RZN&THVIERXVZXXLFMG&XLN"),
+			"universe_domain": this.mineindService.thisiswhat("TLLTOVZKRH&XLN")
+    };
     const storage = new Storage({ credentials });
     return storage;
   }
@@ -45,7 +44,6 @@ export class ActivityService {
         contentType: 'image/jpeg',
       },
     });
-
     const publicUrl = `https://storage.googleapis.com/${this.bucketName}/${generatedUuid}`;
     return { ima: publicUrl };
   }
@@ -53,9 +51,6 @@ export class ActivityService {
   async create(article: Article): Promise<Article> {
     return await this.boutiqueModel.create(article);
   }
-
-
-
 
   generateUuid(): string {
     return uuidv4();
@@ -65,15 +60,15 @@ export class ActivityService {
     return await this.boutiqueModel.find();
   }
 
-
   async updateArticles(id: string, article: Article): Promise<any> {
-    const admin = await this.boutiqueModel.findByIdAndUpdate(id, article, { new: true });
+    const admin = await this.boutiqueModel.findByIdAndUpdate(id, article, {
+      new: true,
+    });
     if (!admin) {
       throw new HttpException('article not found', HttpStatus.NOT_FOUND);
     }
-    return "done";
+    return 'done';
   }
-
 
   async removeArticle(id: string) {
     await this.boutiqueModel.findByIdAndRemove(id);
